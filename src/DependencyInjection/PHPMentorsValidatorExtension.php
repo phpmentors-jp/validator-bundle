3a1f668142f2d87bedf2af9c34e6e18235476ddc
KUBO Atsuhiro <kubo@iteman.jp>
2015-04-16 16:50:47 +0900

    remove invalid argument from the Configuration constructor call

--- src/DependencyInjection/PHPMentorsValidatorExtension.php
+++ src/DependencyInjection/PHPMentorsValidatorExtension.php
@@ -25,7 +25,7 @@ class PHPMentorsValidatorExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $config = $this->processConfiguration(new Configuration($this->kernel), $configs);
+        $config = $this->processConfiguration(new Configuration(), $configs);
 
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
         $loader->load('services.yml');
