62030eb4f3a86ba6a83a53c92a73941fd72dd46d
KUBO Atsuhiro <kubo@iteman.jp>
2015-06-10 01:22:49 +0900

    rename the service name for `ServiceCallback` to `phpmentors_validator.service_callback_validator`

--- src/Constraints/ServiceCallback.php
+++ src/Constraints/ServiceCallback.php
@@ -38,6 +38,6 @@ class ServiceCallback extends Constraint
      */
     public function validatedBy()
     {
-        return 'phpmentors_service_callback';
+        return 'phpmentors_validator.service_callback_validator';
     }
 }
